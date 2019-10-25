bc3e36bf8650ac9191b43d4d014c8b45b51274c2
Philipp Niederlag <philipp.niederlag@artcom.de>
2019-10-25 10:13:40 +0200

    Initial commit for alpha blending branch

--- example_model.py
+++ example_model.py
@@ -1,27 +1,8 @@
-# MIT License
-
-# Copyright (c) 2019 Runway AI, Inc
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import random
 from PIL import Image
+import numpy as np
+from numpy import array
+import cv2
 
 class ExampleModel():
 
@@ -30,16 +11,54 @@ class ExampleModel():
         self.truncation = options['truncation']
 
     # Generate an image based on some text.
-    def run_on_input(self, caption_text):
+    def run_on_input(self, im):
 
-        # This is an example of how you could use some input from
-        # @runway.setup(), like options['truncation'], later inside a
-        # function called by @runway.command().
-        text = caption_text[0:self.truncation]
+        # image = np.array(im)
+        # print(type(image))
+        # print(image.dtype)
+        # print(image.shape)
+        # b,g,r,a = cv2.split(image)
+        # print(a.shape)
+        # alpha = cv2.merge((a,a,a,a))
+        print("shape: {}, mode: {}, info:{}".format(im.size, im.mode, im.info))
 
-        # Return a red image if the input text is "red",
-        # otherwise return a blue image.
-        if text == 'red':
-            return Image.new('RGB', (512, 512), color = 'red')
-        else:
-            return Image.new('RGB', (512, 512), color = 'blue')
+        image = np.array(im.getdata(), np.uint8).reshape(im.size[1], im.size[0], 4)
+        # arr = array(img)
+        
+        # Read the foreground image with alpha channel
+        foreGroundImage = cv2.imread("test.png", -1)
+        foreGroundImage2 = cv2.cvtColor( image, cv2.COLOR_RGBA2BGRA)
+        print("foreGroundImage[...]")
+        print(foreGroundImage[...])
+        print("foreGroundImage2[...]")
+        print(foreGroundImage2[...])
+
+        print("shape: {}, type: {}".format(foreGroundImage.shape, foreGroundImage.dtype))
+        print("shape: {}, type: {}".format(foreGroundImage2.shape, foreGroundImage2.dtype))
+
+
+        # Split png foreground image
+        b,g,r,a = cv2.split(foreGroundImage2)
+
+        # Save the foregroung RGB content into a single object
+        foreground = cv2.merge((b,g,r))
+
+        # Save the alpha information into a single Mat
+        alpha = cv2.merge((a,a,a))
+
+        # Read background image
+        background = cv2.imread("backGround.jpg")
+
+        # Convert uint8 to float
+        foreground = foreground.astype(float)
+        background = background.astype(float)
+        alpha = alpha.astype(float)/255
+
+        # Perform alpha blending
+        foreground = cv2.multiply(alpha, foreground)
+        background = cv2.multiply(1.0 - alpha, background)
+        outImage = cv2.add(foreground, background)
+
+
+        return outImage
+        # return image[:,:,3]
--- runway_model.py
+++ runway_model.py
@@ -1,41 +1,6 @@
-# MIT License
-
-# Copyright (c) 2019 Runway AI, Inc
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# =========================================================================
-
-# This example contains the minimum specifications and requirements
-# to port a machine learning model to Runway.
-
-# For more instructions on how to port a model to Runway, see the Runway Model
-# SDK docs at https://sdk.runwayml.com
-
-# RUNWAY
-# www.runwayml.com
-# hello@runwayml.com
-
-# =========================================================================
-
 # Import the Runway SDK. Please install it first with
 # `pip install runway-python`.
+# `pip install opencv-contrib-python`.
 import runway
 from runway.data_types import number, text, image
 from example_model import ExampleModel
@@ -60,13 +25,13 @@ def setup(opts):
 # inputs and process outputs. To see a complete list of supported inputs and
 # outputs data types: https://sdk.runwayml.com/en/latest/data_types.html
 @runway.command(name='generate',
-                inputs={ 'caption': text() },
-                outputs={ 'image': image(width=512, height=512) },
+                inputs={ 'image': image(channels=4) },
+                outputs={ 'image': image(channels=4) },
                 description='Generates a red square when the input text input is "red".')
 def generate(model, args):
-    print('[GENERATE] Ran with caption value "{}"'.format(args['caption']))
+    print('[GENERATE] Ran with caption value "{}"'.format(args['image']))
     # Generate a PIL or Numpy image based on the input caption, and return it
-    output_image = model.run_on_input(args['caption'])
+    output_image = model.run_on_input(args['image'])
     return {
         'image': output_image
     }
